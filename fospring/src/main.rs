838421ac728e82e0fa016d0bf4de6808eda70a52
qiuyongchun <qiuyongchun@bytedance.com>
2021-05-21 18:57:15 +0800

    feat: update to exec

--- fospring/src/main.rs
+++ fospring/src/main.rs
@@ -1,3 +1,7 @@
+pub fn main() {
+    println!("show_special_string_from_dp: {}", show_special_string_from_dp());
+}
+
 pub fn show_special_string_from_dp() -> String {
     fospring_view::get_special_string()
 }
@@ -10,6 +14,5 @@ mod tests {
         let s = show_special_string_from_dp();
         println!("s={:?}", s);
     }
-
 }
 
