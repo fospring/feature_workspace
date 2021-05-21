5681110439468b03562f953a7007335c99c9986e
qiuyongchun <qiuyongchun@bytedance.com>
2021-05-21 18:26:45 +0800

    init commit

---
+++ fospring-view/src/lib.rs
@@ -0,0 +1,8 @@
+
+pub fn get_special_string() -> String {
+    "feature gate1 on".to_string()
+}
+
+pub fn get_special2_string() -> String {
+    "feature gate2 on".to_string()
+}
---
+++ fospring/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn show_special_string_from_dp() -> String {
+    fospring_view::get_special_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_show_string() {
+        let s = show_special_string_from_dp();
+        println!("s={:?}", s);
+    }
+
+}
+
